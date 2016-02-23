2240340c8cb0866092478a83d906b7ebc7e35657
woogicus <woogicus@gmail.com>
2016-02-23 10:19:41 -0600

    Upgrade system.

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ var game = new Phaser.Game(1200, 600, Phaser.AUTO, 'gameZone', { preload: preloa
 
 function preload() {
 	game.load.spritesheet('basicButton', './flixel-button.png', 80, 20);
+	game.load.spritesheet('upgradeSprites', './UpgradeSprites.png', 74, 73);
+	game.load.image('upgradeHeader', './UpgradeHeader.png');
 }
 
 var countText; 
@@ -13,14 +15,16 @@ var cursorButton; var cursorButtonText;
 var farmButton; var farmButtonText;
 var savedText; var saveGameButton; var saveButtonText;
 var deleteSaveButton; var deleteButtonText;
+var upgradeBanner;
 function create() {
 	game.stage.backgroundColor = 0x8d8dad;
 	//Souls count in the center.
 	countText = game.add.text(550, 300, 'Souls: 0', { fontSize: '48px', fill: '#004444', align: "center"});
 	perSecondText = game.add.text(550, 350, 'Per Second: 0.5', { fontSize: '16px', fill: '#eeeeee', align: "center" });
 	//version tracking number (for less obvious changes)
-	var versionNumber = game.add.text(32, 32, 'Version ALPHA 0.082', { align: 'left'});
+	var versionNumber = game.add.text(32, 32, 'Version ALPHA 0.09', { align: 'left'});
 	savedText = game.add.text(32, 64, '', { align: 'left'});
+	upgradeBanner = game.add.sprite(50, 100, 'upgradeHeader');
 	
 	
 	//Building buttons and corresponding text. 
@@ -77,6 +81,8 @@ var towns = 0;
 var townProd = 15;
 var townMulti = 1;
 var allMulti = 1;
+//Spell info - needs saved
+var currentMana = 0; var totalMana = 100;
 // Building values that don't need saved: Base building cost, total production amount
 var cursorBaseCost = 9;
 var farmBaseCost = 50;
@@ -97,6 +103,7 @@ function incrementByProd(){ //Master game "tick" is 5 times per second.
 	if (autoSave == -1) loadGame();
 	autoSave++;
 	if (autoSave % 10 == 0) savedText.text = ''; // Clear every other second.
+	if (autoSave % 5 == 0 ) resetUpgrades();     // Once per second
 	if(autoSave >= 300) { //Every 60 seconds
 		saveGame();
 		autoSave = 0;
@@ -169,6 +176,85 @@ function buyTown(){
 var purchasedUpgrades = 0;
 var cursorUpgrade10Bought = false;
 var farmUpgrade10Bought = false;
+var townUpgrade10Bought = false;
+
+var upgradeArray [];  // An array of upgrade objects
+//One upgrade: 
+/*
+	spriteIndex: Index for upgrade spritesheet
+	func:        Function to perform when image is clicked
+	sprite:      Sprite object to display
+*/
+function resetUpgrades(){
+//Destroy the previous array if it has anything in it.
+	var index;
+	for (index = upgradeArray.length - 1; index >= 0; index--){
+		upgradeArray[index].sprite.destroy();  //Remove the sprite
+		upgradeArray.pop(); // And trash the rest.
+	}
+//Rebuild the array of upgrade sprites
+	if (cursors >= 10 && !cursorUpgrade10Bought) { // 10 cursors, production x2, cost 50
+		upgradeArray.push({spriteIndex: 0, func: cursorUpgrade10, sprite: 0});
+	//	upgradeText += "<br><button onclick='cursorUpgrade10(50)'>Bronze Cursors - 50</button>";
+        //	upgradeText += 'Increase Cursor output by 100%';
+	}
+	if (farms >= 10 && !farmUpgrade10Bought) { // 10 farms, production x2, cost 250
+	upgradeArray.push({spriteIndex: 1, func: farmUpgrade10, sprite: 0});
+	//	upgradeText += "<br><button onclick='farmUpgrade10(250)'>Bronze Tractors - 250</button>";
+        //	upgradeText += 'Increase Farm output by 100%';
+	}
+	if (towns >= 10 && !townUpgrade10Bought) upgradeArray.push({spriteIndex: 2, func: townUpgrade10, sprite: 0});
+//Display on screen
+	upgradeArray.foreach(displayUpgrade);
+	
+}
+//This is called for each item in the upgrade array.  Use the info passed to add a sprite to the game world.
+var upgradeX = 40; var upgradeY = 150;  //Starter values
+var upgradeSpacingX = 75; var upgradeSpacingY = 75;
+var upgradeMaxX = 340; var upgradeMaxY = 450
+function displayUpgrade(item){
+	if (upgradeY <= upgradeMaxY){  //Stop printing if we run out of space.  TODO: improve?
+		item.sprite = game.add.sprite(upgradeX, upgradeY, 'upgradeSprites', item.spriteIndex);
+		item.sprite.input.useHandCursor = true;
+        	item.sprite.events.onInputDown.add(item.func, this);
+        	upgradeX += upgradeSpacingX;
+        	if (upgradeX > upgradeMaxX) {  //Start a new row
+        		upgradeX = 40;
+        		upgradeY += upgradeSpacingY;
+		}
+	}   
+}
+
+//Upgrade functions go here.
+function cursorUpgrade10(){
+	if (soulsCount >= 50){
+		soulsCount -= 50;    //Pay the cost...
+		cursorMulti *= 2; //Increase the multiplier...
+		cursorUpgrade10Bought = true; //Disable the button.
+		purchasedUpgrades++; //And increment the upgrade counter.
+		resetUpgrades();
+	}
+}
+function farmUpgrade10(){
+	if (soulsCount >= 250){
+		soulsCount -= 250;    //Pay the cost...
+		farmMulti *= 2; //Increase the multiplier...
+		farmUpgrade10Bought = true; //Disable the button.
+		purchasedUpgrades++; //And increment the upgrade counter.
+		resetUpgrades();
+	}
+}
+function townUpgrade10(){
+	if (soulsCount >= 1500){
+		soulsCount -= 1500;    //Pay the cost...
+		townMulti *= 2; //Increase the multiplier...
+		townUpgrade10Bought = true; //Disable the button.
+		purchasedUpgrades++; //And increment the upgrade counter.
+		resetUpgrades();
+	}
+}
+//Spell functions: TODO
+
 
 // Save and load functions.
 function saveGame(){
@@ -180,9 +266,13 @@ function saveGame(){
 		farmMulti: farmMulti,
 		towns: towns,
 		townMulti: townMulti,
+		allMulti: allMulti,
+		currentMana: currentMana;
+		totalMana: totalMana;
 		purchasedUpgrades: purchasedUpgrades,
 		cursorUpgrade10Bought: cursorUpgrade10Bought,
-		farmUpgrade10Bought: farmUpgrade10Bought
+		farmUpgrade10Bought: farmUpgrade10Bought,
+		townUpgrade10Bought: townUpgrade10Bought
 	}
 	var success = true;
 	try{
@@ -211,10 +301,15 @@ function loadGame(){
 		if (typeof savegame.farmMulti !== "undefined") farmMulti = savegame.farmMulti;
 		if (typeof savegame.towns !== "undefined") towns = savegame.towns;
 		if (typeof savegame.townMulti !== "undefined") townMulti = savegame.townMulti;	
+		if (typeof savegame.allMulti !== "undefined") allMulti = savegame.allMulti;
+		//Spell info
+		if (typeof savegame.currentMana !== "undefined") currentMana = savegame.currentMana;
+		if (typeof savegame.totalMana !== "undefined") totalMana = savegame.totalMana;
 		// Upgrade status
 		if (typeof savegame.purchasedUpgrades !== "undefined") purchasedUpgrades = savegame.purchasedUpgrades;
 		if (typeof savegame.cursorUpgrade10Bought !== "undefined") cursorUpgrade10Bought = savegame.cursorUpgrade10Bought;
 		if (typeof savegame.farmUpgrade10Bought !== "undefined") farmUpgrade10Bought = savegame.farmUpgrade10Bought;
+		if (typeof savegame.townUpgrade10Bought !== "undefined") townUpgrade10Bought = savegame.townUpgrade10Bought;
 		console.log("game loaded!");
 		resetButtons();
 		 
@@ -229,5 +324,8 @@ function deleteSave(){
 	cursorMulti = 1;
 	farmMulti = 1;
 	townMulti = 1;
+	allMulti = 1;
+	currentMana = 0;
+	totalMana = 100;
 	resetButtons();
 }
